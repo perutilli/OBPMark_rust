c7010bc3a7d362a544fac8cc73fe84eb7dfd772c
Alberto Perugini s301240 <s301240@studenti.polito.it>
2023-08-19 23:47:07 +0200

    various fixes from merge

--- reference_algorithms/build.rs
+++ reference_algorithms/build.rs
@@ -6,17 +6,24 @@ const C_TYPE: &str = "FLOAT";
 const C_TYPE: &str = "DOUBLE";
 #[cfg(feature = "int")]
 const C_TYPE: &str = "INT";
-#[cfg(not(any(feature = "float", feature = "double", feature = "int")))]
+#[cfg(feature = "half")]
+compile_error!("Half precision is not supported yet");
+#[cfg(not(any(
+    feature = "float",
+    feature = "double",
+    feature = "int",
+    feature = "half"
+)))]
 const C_TYPE: &str = "FLOAT";
 
 fn main() {
     cc::Build::new()
-        .file("reference_versions/cpu_functions.c")
+        .file("src/cpu_functions.c")
         .define(C_TYPE, None)
         .flag("-O3")
         .warnings(false)
-        .compile("reference_functions");
+        .compile("reference_algorithms");
 
     println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=reference_versions/cpu_functions.c");
+    println!("cargo:rerun-if-changed=src/cpu_functions.c");
 }
